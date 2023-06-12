5e2eb3cb758562f6b89115f445fd0f89cf75b02c
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2023-06-12 03:16:24 -0600

    App working!

--- CounterAppUIkit/ViewController.swift
+++ CounterAppUIkit/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
+    
+    func updateUI() {
+        counterLabel.text = "\(counter)"
+    }
+    
+    @IBAction func refreshButton(_ sender: UIButton) {
+        counter = 0
+        updateUI()
+    }
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        counter += 1
+        updateUI()
+    }
+    
+    @IBAction func minusButton(_ sender: UIButton) {
+        counter -= 1
+        updateUI()
+    }
+    
 
 }
 
